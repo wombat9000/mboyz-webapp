ce95c35f56af230d1a3d432cc03de83185d622be
bstein <bstein@thoughtworks.com>
2017-04-08 02:26:07 +0200

    add gradle one jar which seems to include static resources aswell

--- src/main/kotlin/org/mboyz/holidayplanner/Server.kt
+++ src/main/kotlin/org/mboyz/holidayplanner/Server.kt
@@ -1,6 +1,5 @@
 package org.mboyz.holidayplanner
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
@@ -8,10 +7,6 @@ import org.springframework.context.annotation.ComponentScan
 import org.springframework.boot.web.support.SpringBootServletInitializer
 import org.springframework.boot.builder.SpringApplicationBuilder
 import java.util.HashMap
-
-
-
-
 
 @ComponentScan
 @EnableAutoConfiguration(exclude = arrayOf(DataSourceAutoConfiguration::class))
@@ -21,23 +16,17 @@ open class Server : SpringBootServletInitializer(){
     companion object {
         @JvmStatic fun main(args: Array<String>) {
             val getenv: String? = System.getenv("PORT")
-
             var PORT = 8080
-
             if (getenv != null) {
                 PORT = Integer.valueOf(getenv)
             }
 
             val props = HashMap<String, Any>()
             props.put("server.port", PORT)
-
-//            SpringApplication.run(Server::class.java, *args)
-
             SpringApplicationBuilder()
                     .sources(Server::class.java)
                     .properties(props)
                     .run(*args)
         }
     }
-
 }
