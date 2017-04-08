d51d1dded6841e7c94442bdd778ca133fd2265f2
bstein <bstein@thoughtworks.com>
2017-04-08 14:01:22 +0200

    cert challenge

--- src/main/kotlin/org/mboyz/holidayplanner/HolidayController.kt
+++ src/main/kotlin/org/mboyz/holidayplanner/HolidayController.kt
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
 
 
 @Controller
@@ -14,5 +15,11 @@ class HolidayController() {
     fun hello(model: Model, @RequestParam(value = "name", required = false, defaultValue = "World") name: String): String {
         model.addAttribute("name", "bastian")
         return "hello"
+    }
+
+    @RequestMapping("/.well-known/acme-challenge/OOoxUBhW6UNzrsy4nS3blt-6y39ixWSUJxLHKxj_hYg")
+    @ResponseBody
+    fun acmeChallenge(): String {
+        return "OOoxUBhW6UNzrsy4nS3blt-6y39ixWSUJxLHKxj_hYg.-_7eA0VFURUq6JOdLnLrAilww8Z4w7K-ekp7wX8e7vw"
     }
 }
