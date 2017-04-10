022c95ff71ec15a3cf5a847d3c42a255233aab22
bstein <bstein@thoughtworks.com>
2017-04-10 22:28:55 +0200

    add missing reducer test

--- src/test/js/reducers/holiday.test.js
+++ src/test/js/reducers/holiday.test.js
@@ -1,14 +1,28 @@
 import holiday from '../../../main/resources/static/js_src/reducers/holidays.es6';
 
-test('should add holidays', function () {
 
-	const action = {
-		type: "ADD_HOLIDAY",
-		holiday: "someHoliday"
-	};
+describe('holiday reducer', function () {
+	it('should add holidays', function () {
+		const action = {
+			type: "ADD_HOLIDAY",
+			holiday: "someHoliday"
+		};
 
-	const state = holiday([], action);
+		const state = holiday([], action);
 
-	expect(state.length).toBe(1);
-	expect(state[0]).toBe("someHoliday");
+		expect(state.length).toBe(1);
+		expect(state[0]).toBe("someHoliday");
+	});
+
+	it('should default to [] as initial state', function () {
+	    const unknownAction = {
+	    	type: "unknown",
+		    holiday: "someHoliday"
+	    };
+
+		const state = holiday(undefined, unknownAction);
+
+		expect(state).toEqual([]);
+	});
 });
+
