9c4fb12759d866e52a2829481bc4a7a5bcc95779
bstein <bstein@thoughtworks.com>
2017-05-04 23:35:55 +0200

    remove unused user class

--- src/main/kotlin/org/mboyz/holidayplanner/user/User.kt
+++
@@ -1,7 +0,0 @@
-package org.mboyz.holidayplanner.user
-
-data class User(val id: Int?,
-                val firstName: String,
-                val lastName: String,
-                val email: String,
-                val passwordHash: String)
