3a03771ddab941190d3c5c0e8131fbaa6ac7a41c
bstein <bstein@thoughtworks.com>
2017-07-23 23:00:50 +0200

    add holiday detail view

--- src/main/kotlin/org/mboyz/holidayplanner/holiday/HolidayPagesController.kt
+++ src/main/kotlin/org/mboyz/holidayplanner/holiday/HolidayPagesController.kt
@@ -3,8 +3,10 @@ package org.mboyz.holidayplanner.holiday
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.servlet.ModelAndView
 
 @Controller
 @RequestMapping("/holiday")
@@ -18,5 +20,11 @@ class HolidayPagesController(@Autowired val holidayRepository: HolidayRepository
         @RequestMapping(value = "/", method = arrayOf(RequestMethod.GET))
         fun index(): String {
             return "holidayOverview"
+        }
+
+    @RequestMapping(value = "/{id}", method = arrayOf(RequestMethod.GET))
+        fun detail(@PathVariable id: Long): ModelAndView {
+            val holiday = holidayRepository.findOne(id)
+            return ModelAndView("holidayDetailView", "holiday", holiday)
         }
 }
