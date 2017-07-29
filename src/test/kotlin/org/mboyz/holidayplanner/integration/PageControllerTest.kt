c6d7ab59103ff4644cdcaa77bcc2639417455d6f
bstein <bstein@thoughtworks.com>
2017-07-29 21:54:08 +0200

    fix page controller test

--- src/test/kotlin/org/mboyz/holidayplanner/integration/PageControllerTest.kt
+++ src/test/kotlin/org/mboyz/holidayplanner/integration/PageControllerTest.kt
@@ -1,19 +1,29 @@
 package org.mboyz.holidayplanner.integration
 
+import org.junit.Before
 import org.junit.Test
-import org.springframework.beans.factory.annotation.Autowired
+import org.mboyz.holidayplanner.web.PageController
+import org.mockito.MockitoAnnotations
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.view
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
 
-class PageControllerTest: AbstractSpringTest() {
-    @Autowired
-    lateinit var mvc: MockMvc
+class PageControllerTest {
 
+    lateinit var testee: PageController
+    lateinit var mockMvc: MockMvc
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        testee = PageController()
+        mockMvc = MockMvcBuilders.standaloneSetup(testee).build()
+    }
     @Test
     fun shouldShowHomePage() {
-        mvc.perform(MockMvcRequestBuilders.get("/"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
                 .andExpect(status().isOk)
                 .andExpect(view().name("home"))
     }
