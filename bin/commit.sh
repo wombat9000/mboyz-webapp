69357dec81d0da2e580ce7b2b03bb4c6c57c7f3b
bstein <bstein@thoughtworks.com>
2017-07-31 23:41:22 +0200

    modify commit script

--- bin/commit.sh
+++ bin/commit.sh
@@ -1,1 +1,19 @@
 #!/usr/bin/env bash
+set -e
+
+changed=$(git status --porcelain)
+
+if [ "$changed" != "" ]; then
+
+	echo -e "Git Status:"
+	git status -s
+	echo ""
+
+	echo "All changes will be added to commit after entering message:"
+	read -p "message: " -e message
+
+	git add .
+
+	git commit -m "${message}"
+
+fi
