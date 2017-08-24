0e5f55190f98c2bfc5e8dcb1fd04bc4f874bc438
Bastian Stein <stone.bastian@gmail.com>
2017-08-25 00:29:57 +0200

    move datasource config into property files. make timestamp compatible with h2 db

--- src/test/kotlin/org/mboyz/holidayplanner/integration/AbstractSpringTest.kt
+++ src/test/kotlin/org/mboyz/holidayplanner/integration/AbstractSpringTest.kt
@@ -3,49 +3,40 @@ package org.mboyz.holidayplanner.integration
 import org.flywaydb.core.Flyway
 import org.junit.After
 import org.junit.Before
-import org.junit.BeforeClass
 import org.junit.runner.RunWith
 import org.mboyz.holidayplanner.holiday.HolidayService
 import org.mboyz.holidayplanner.holiday.participation.ParticipationRepository
 import org.mboyz.holidayplanner.user.UserService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.PropertySource
-import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
+import javax.annotation.PostConstruct
+import javax.sql.DataSource
 
 @RunWith(SpringRunner::class)
 @PropertySource("classpath:secret.properties", ignoreResourceNotFound = true)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles(profiles = arrayOf("local"))
 @AutoConfigureMockMvc
 abstract class AbstractSpringTest {
-    companion object {
-        @BeforeClass
-        @JvmStatic
-        fun performMigrations() {
-            val build = DataSourceBuilder
-                    .create()
-                    .username("sa")
-                    .url("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE;MODE=PostgreSQL")
-                    .driverClassName("org.h2.Driver")
-                    .build()
 
-            val flyway = Flyway()
-            flyway.dataSource = build
-            flyway.clean()
-            flyway.migrate()
-        }
-    }
-
+    @Autowired
+    lateinit var dataSource: DataSource
     @Autowired
     lateinit var userService: UserService
     @Autowired
     lateinit var holidayService: HolidayService
     @Autowired
     lateinit var participationRepository: ParticipationRepository
+
+    @PostConstruct
+    fun performMigrations() {
+        val flyway = Flyway()
+        flyway.dataSource = dataSource
+        flyway.clean()
+        flyway.migrate()
+    }
 
     @Before
     fun setup() {
