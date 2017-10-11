a649e778476ccbff8afcdf89cf71f3871428731a
bstein <bstein@thoughtworks.com>
2017-10-11 21:27:12 +0200

    use htmlwebpack plugin

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,5 +1,12 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+	template: './src/index.html',
+	filename: 'index.html',
+	inject: 'body'
+});
+
 module.exports = {
 	entry: './src/index.js',
 	devtool: 'inline-source-map',
@@ -9,8 +16,9 @@ module.exports = {
 	},
 	module: {
 		loaders: [
-			{ test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-			{ test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+			{test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/},
+			{test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/}
 		]
-	}
+	},
+	plugins: [HtmlWebpackPluginConfig]
 };
